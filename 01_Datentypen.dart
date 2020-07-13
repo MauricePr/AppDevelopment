7d5d6d2f0949fd8ff090e54cda9e34f46b23bb72
Maurice Pradel <68158829+MauricePr@users.noreply.github.com>
2020-07-13 14:44:48 +0200

    Create 01_Datentypen.dart

---
+++ 01_Datentypen.dart
@@ -0,0 +1,18 @@
+main() {
+  
+  int Zahl1 = 1;
+  int Zahl2 = 2;
+  print(Zahl1);
+  print(Zahl2);
+  
+  double Kommazahl1 = 1.5;
+  double Kommazahl2 = 2.5;
+  print(Kommazahl1);
+  print(Kommazahl2);
+  
+  
+  String Text1 = "Hello World";
+  print(Text1);
+  
+  
+ }
