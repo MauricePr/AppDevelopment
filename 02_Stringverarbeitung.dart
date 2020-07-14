c821724bbe6faa5c6830b5b06c11858e8f88e6ba
Maurice Pradel <68158829+MauricePr@users.noreply.github.com>
2020-07-14 10:33:32 +0200

    Create 02_Stringverarbeitung.dart

---
+++ 02_Stringverarbeitung.dart
@@ -0,0 +1,14 @@
+main () {
+  
+  String meinName = "Maurice";
+  print("$meinName hat gewonnen!");
+  
+  double kommazahl = 0.5;
+  String text = kommazahl.toString();
+  print(text);
+  
+  int laengeMeinName = meinName.length;
+  print(laengeMeinName);
+  
+  
+  }
